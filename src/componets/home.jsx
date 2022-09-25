c1f57ce11d4a3092b6d423a714101b470e65ca20
mohamed-dellai <mohameddellai6@gmail.com>
2022-09-25 12:37:04 +0200

    Add existing project files to Git

--- src/componets/home.jsx
+++ src/componets/home.jsx
@@ -2,41 +2,52 @@ import smile from '../smile-svgrepo-com.svg'
 import auth from '../globalstate.js/global.jsx'
 import { proxy } from "valtio"
 import { useNavigate } from 'react-router-dom'
+import heart from '../red-heart-icon.svg'
+import up from '../image-up.svg'
 import axios from 'axios'
 import { useEffect,useState } from 'react'
 
 
+
 export function Home(){
-  const snapshot=proxy(auth)
   
+  const snapshot=proxy(auth)
   const [userName,setUserName]=useState("")
   const [post,setPosts]=useState([])
   const[clicks,setClicks]=useState(()=>[])
+  
   useEffect(()=>{
-    setTimeout(()=>{getPosts()},500)
+    const timer=setTimeout(()=>{getPosts()},1000)
+    return () => clearTimeout(timer);
     
   })
-
+  const like=async (e)=>{
+    const id=e.target.parentElement.id
+    const t=e.currentTarget
+    
+    t.style.color="red"
+    setTimeout((t)=>{t.style.color="black"},500)
+    const data={
+      id:id,
+      email:snapshot.email
+    }
+    
+    var result=await axios.post("http://localhost:5000/home/like",data)
+    console.log(result)
+    
+  }
   const sendLike=async (id)=>{
       const data={
         id:id,
         email:snapshot.email
       }
-      console.log(data)
+      
       var result=await axios.post("http://localhost:5000/home/like",data)
       console.log(result)
   }
  
- useEffect(()=>{
-  setTimeout(()=>{
-    let newArr=clicks
-    for(let i=0;i<clicks.length;i++){
-      
-      newArr[i]=0
-    }
-    setClicks(newArr)
-  },500)
- })
+
+
   const handleClick=(e)=>{
     if(e.path[0].className==="post")
       {
@@ -44,9 +55,14 @@ export function Home(){
         let arr=clicks
         
         if(arr[parseInt(e.path[2].id)]===1){
-          console.log('added')
+          
           sendLike(parseInt(e.path[2].id))
           arr[parseInt(e.path[2].id)]=0
+          e.path[1].children[1].style.opacity="1"
+          e.path[1].children[1].style.width="100px"
+          
+          setTimeout(()=>{e.path[1].children[1].style.opacity="0";e.path[1].children[1].style.width="50px"},500)
+          
 
         }
         else
@@ -54,6 +70,30 @@ export function Home(){
           
      }
     
+  }
+
+
+  const renderImages=(arr)=>{
+     const allimageContainer=document.getElementsByClassName('middle')
+     
+     for(let k=0;k<arr.data.length;k++){
+      
+      if(arr.data[k].photo===null){
+        
+        continue;
+      }
+      if(allimageContainer[k].children.length<2){
+      let img = document.createElement("img");
+      img.src=arr.data[k].photo
+      img.classList.add("post")
+      allimageContainer[k].prepend(img)
+      console.log(allimageContainer[k].children.length)
+      }
+      
+     }
+    
+     
+     return;
   }
   useEffect(()=>{
     
@@ -69,43 +109,78 @@ export function Home(){
       const posts=await axios.get(`http://localhost:5000/home`)
       const userInfo=await axios.get(`http://localhost:5000/home:${snapshot.email}`)
       setPosts(posts.data)
-      setUserName(userInfo.data[0].name)
       
-    
-    
-    
-  }
+      setUserName(userInfo.data[0].name)
+      renderImages(posts)
+    }
   catch(e){
     console.log(e)
   }
+  let newArr=clicks
+
+    for(let i=0;i<clicks.length;i++){
+      
+      newArr[i]=0
+    }
+    setClicks(newArr)
   }
+ 
   const navigate=useNavigate()
   
   if(snapshot.authen===false){
     setTimeout(()=>{navigate("/")} , 3500)
     return <h1 style={{color: "red" , width:'fit-content' , margin: "auto", marginTop:'20%',fontSize:"50px"}}> You are not logged In redirecting....</h1> 
   }
+  
+  const sendPost=async (date,e)=>{
 
+    const data={value:e.target.result, email:snapshot.email, text:document.getElementById('share').value, date:date}
+    const result=await axios.post("http://localhost:5000/upload",data)
+    console.log(result.data)
+    
+  }
 
+  const logFile=(e)=>{
+    const time = new Date();
+    let date=`${time.getDate()}-${time.getMonth()}-${time.getFullYear()} ${time.getHours()}:${time.getMinutes()}`
+
+     sendPost(date,e)
+  }
+
+  const handleFile=()=>{
+    const reader = new FileReader();
+    let file=document.getElementById("input-file").files[0];
+    reader.onload = logFile;
+    reader.readAsDataURL(file)
+  }
  
  
   
-
+   
     return(
+        
         <div className='home' style={{background: "white" }}>
         <div className="navigation">
 
     <div className="profile">
-       <img className="img" src="https://cdn2.bigcommerce.com/server5400/3po1k2/products/8171/images/14559/161_light_blue__46032.1418747956.1280.1280.jpg" width="25px" height="25px" alt=""/>
+       <img className="profile-pic" src="https://cdn2.bigcommerce.com/server5400/3po1k2/products/8171/images/14559/161_light_blue__46032.1418747956.1280.1280.jpg" width="25px" height="25px" alt=""/>
        <p>{userName}</p>
     </div>
-    
-
   </div>
 
 
-<div className="suggestions">
-
+<div className="share">
+   <div className='share-container'>
+      
+         <img className="profile-pic"  src="https://cdn2.bigcommerce.com/server5400/3po1k2/products/8171/images/14559/161_light_blue__46032.1418747956.1280.1280.jpg" width="25px" height="25px" alt=""/>
+         <input type='text' placeholder={`what's new ${userName}`} id="share"></input>
+         <img src={up} style={{cursor:"pointer" , width:'28px'}} alt='upload' className='up-img' onClick={()=>{document.getElementById('input-file').click()}}></img>
+         <input type='file' id='input-file' alt='upload-image'></input>
+         <button className="button-43" onClick={handleFile}>Share</button>
+      
+   </div>
+   
+   
 </div>
 
 <div className="tumbler">
@@ -113,7 +188,7 @@ export function Home(){
  {
  post.map((obj,count)=>{
   return(
-    <div className="container" id={obj.id}>
+    <div className="container" key={count} id={obj.id}>
 
     <div className="top">
 
@@ -124,7 +199,7 @@ export function Home(){
       <div className="information">
         <div className="username">
           <div className="designation"> {obj.name} </div>
-          <div className="blue-tick">  </div>
+          
         </div>
         <div className="name">{obj.date}</div>
       </div>
@@ -135,21 +210,15 @@ export function Home(){
 
     </div>
 
-    <div className="middle" >
-      <img src="https://cdn2.bigcommerce.com/server5400/3po1k2/products/8171/images/14559/161_light_blue__46032.1418747956.1280.1280.jpg?c=2g" className="post" alt=""/>
-    </div>
+    <div className="middle"  >
 
-    <div className="options">
-      <div className="buttons">
-
-     
       
-
-      </div>
-
+      <img className="heart" src={heart} alt="like"></img>
     </div>
 
-    <div className="likes">{obj.likes} likes</div>
+    
+
+    <div className="likes"  onClick={like}>{obj.likes} likes</div>
 
     <div className="caption">
       <div className="content">
